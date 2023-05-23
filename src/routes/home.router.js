296634f3bf18521b2799ea2c2883748bfe1c7b28
szulz <ezeszulz@hotmail.com>
2023-05-23 18:08:21 -0300

    modified:   src/db/productManager.json 	new file:   src/public/js/home.js 	modified:   src/routes/home.router.js 	modified:   src/views/home.handlebars 	modified:   src/views/layouts/main.handlebars 	new file:   src/views/realTimeProducts.handlebars 	deleted:    src/views/usuarios.handlebars

--- src/routes/home.router.js
+++ src/routes/home.router.js
@@ -4,15 +4,19 @@ const homeRouter = express.Router()
 const ProductManager = require('../controllers/productManager.js')
 const productManager = new ProductManager();
 
+homeRouter.get("/realtimeproducts", async (req, res) => {
+    let productos = await productManager.getProducts();
+    return res.render("realTimeProducts", {productos})
+})
+
 homeRouter.get("/", async (req, res) => {
     let productos = await productManager.getProducts();
     return res.render("home", {productos})
 })
 
-homeRouter.post('/', async (req, res) => {
-    console.log('acá estoy')
-    let myBody = req.body
-    res.send(myBody)
+homeRouter.post("/", async (req, res) =>{
+    console.log('ENTRÓ AL POST');
 })
+
 
 module.exports = homeRouter
